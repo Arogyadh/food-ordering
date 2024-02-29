2d09efaab0511258e3cf652a81aa06472d524640
unknown <arogyadhdl@gmail.com>
2024-02-29 22:32:20 +0545

    fixed minor build errors

--- src/components/layout/AboutUs.js
+++ src/components/layout/AboutUs.js
@@ -6,17 +6,17 @@ export default function AboutUs() {
       <SectionHeaders subHeader={"Our Story"} mainHeader={"About Us"} />
       <div className="max-w-xl mx-auto mt-4 text-gray-500 flex flex-col gap-4">
         <p>
-          Welcome to{" "}
+          Welcome to
           <span className="font-bold">
             TC<sup>2</sup>
           </span>
-          . We're here to make ordering food a breeze. Choose from a wide range
-          of local and global cuisines, and let us handle the rest.
+          . We&apos;re here to make ordering food a breeze. Choose from a wide
+          range of local and global cuisines, and let us handle the rest.
         </p>
         <p className="max-w-2xl mx-auto mt-4 text-gray-500">
-          Whether it's a quick bite or a special treat, we've got you covered.
-          Ordering is easy, fast, and reliable. So, sit back, relax, and enjoy
-          your meal!
+          Whether it&apos;s a quick bite or a special treat, we&apos;ve got you
+          covered. Ordering is easy, fast, and reliable. So, sit back, relax,
+          and enjoy your meal!
         </p>
       </div>
     </section>
